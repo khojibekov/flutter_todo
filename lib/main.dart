4ebd0d69db463e2749336bd6fb0cb2dcbce0752b
khojibekov <khojibekovtim@gmail.com>
2021-11-11 11:50:50 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:firebase_core/firebase_core.dart ';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home.dart';
+import 'package:flutter_todo/pages/main_screen.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.deepOrangeAccent,
+    ),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MainScreen(),
+      '/todo': (context) => Home(),
+    },
+  ));
+}
